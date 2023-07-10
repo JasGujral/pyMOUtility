f714b40e8897a4ed700d1f2164619cd70eca9e8c
JasGujral <jasmeet.gujral@mostacktechnology.com>
2023-07-10 11:57:11 +0530

    WIP - core code added. Examples and testcases pending.

---
+++ README.md
@@ -0,0 +1,66 @@
+# 🚀 PyUtility
+
+A handy collection of helper functions, decorators, and utilities designed to streamline and simplify your Python 🐍 development experience. It provides powerful tools to make your code cleaner, more readable, and easier to debug.
+
+## ⚙️ Installation
+
+To install pyUtility, you can use pip:
+
+```
+pip install pyUtility
+```
+
+Make sure you have Python 3.7 or higher installed on your system.
+
+## ✨ Features
+
+* **🛠️ Helper Functions**: A collection of functions to help handle and manipulate data more efficiently.
+* **🖼️ Decorators**: Save time on writing repetitive code blocks with these ready-to-use decorators.
+* **🔍 Error Handlers**: Tools for handling and debugging exceptions gracefully.
+
+## 📖 Usage
+
+Import the required utility from pyUtility package. Here's a simple example:
+
+```python
+from pyUtility.decorators import timing
+
+@timing
+def my_function():
+    ...
+
+my_function() # this will print the time taken to execute my_function
+```
+
+## 📚 Detailed Documentation
+
+For a complete understanding of how to use pyUtility, refer to the [documentation](LINK_TO_DOCUMENTATION_HERE).
+
+## 💡 Examples
+
+To see pyUtility in action, check out these [examples](LINK_TO_EXAMPLES_HERE).
+
+## 👥 Contributing
+
+Contributions are welcome! See our [contributor's guide](LINK_TO_CONTRIBUTING_GUIDE) for details on how you can help improve pyUtility.
+
+## 🧪 Testing
+
+To run the tests, use the following command:
+
+```
+python -m pytest "tests"
+```
+
+## 📜 Changelog
+
+See the [CHANGELOG]() for a history of notable changes to pyUtility.
+
+## ⚖️ License
+
+This project is licensed under the MIT License - see the [LICENSE](./LICENSELICENSE) file for details.
+
+## 📧 Contact
+
+For any queries, feel free to open an issue or contact us at [jasmeet.gujral@mostacktechnology.com](mailto:pjasmeet.gujral@mostacktechnology.com).
+```
---
+++ pyutility/src/debugger.py
@@ -0,0 +1,116 @@
+"""Utilities to help you debug your code better."""
+
+
+from typing import Callable, Any, Union, Dict
+from functools import wraps
+import logging
+import time
+
+
+def timeit(output: Union[logging.Logger, Callable[..., None]]):
+    """
+    Decorator to log the start, end and runtime of a function.
+
+    Args:
+        output: Logger instance or print function used for output.
+    """
+    def decorator_log_time(func: Callable):
+        """
+        Decorator function.
+        """
+        @wraps(func)
+        def wrapper_log_time(*args, **kwargs) -> Any:
+            """
+            Wrapper function.
+            """
+            start_time = time.time()
+            
+            if isinstance(output, logging.Logger):
+                output.info(f'Started execution of {func.__name__} function')
+            else:
+                output(f'Started execution of {func.__name__} function')
+            
+            result = func(*args, **kwargs)
+            
+            end_time = time.time()
+            run_time = end_time - start_time
+
+            if isinstance(output, logging.Logger):
+                output.info(f'Finished execution of {func.__name__} function, runtime: {run_time} seconds')
+            else:
+                output(f'Finished execution of {func.__name__} function, runtime: {run_time} seconds')
+
+            return result
+
+        return wrapper_log_time
+
+    return decorator_log_time
+
+
+def debugger(output: Union[logging.Logger, Callable[..., None]]):
+    """
+    Decorator for debugging purposes.
+
+    Logs the function name, arguments and output.
+
+    Args:
+        output: Logger instance or print function used for output.
+    """
+    def decorator_debug(func: Callable):
+        """
+        Decorator function.
+        """
+        @wraps(func)
+        def wrapper_debug(*args, **kwargs) -> Any:
+            """
+            Wrapper function.
+            """
+            args_repr = [repr(a) for a in args]                      
+            kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  
+            signature = ", ".join(args_repr + kwargs_repr)           
+            
+            if isinstance(output, logging.Logger):
+                output.debug(f"Calling {func.__name__}({signature})")
+            else:
+                output(f"Calling {func.__name__}({signature})")
+
+            result = func(*args, **kwargs)
+
+            if isinstance(output, logging.Logger):
+                output.debug(f"{func.__name__!r} returned {result!r}")   
+            else:
+                output(f"{func.__name__!r} returned {result!r}") 
+
+            return result
+
+        return wrapper_debug
+
+    return decorator_debug
+
+
+class FunctionCounter:
+    """
+    A class that keeps a count of calls to multiple functions.
+
+    When used as a decorator, it increments the count for the
+    decorated function each time the function is called.
+    """
+    function_counts: Dict[str, int] = {}
+
+    def __init__(self, func: Callable[..., Any]):
+        self.func: Callable[..., Any] = func
+        self.function_counts[func.__name__] = 0
+
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        self.function_counts[self.func.__name__] += 1
+        print(f"Call {self.function_counts[self.func.__name__]} of {self.func.__name__!r}")
+        return self.func(*args, **kwargs)
+
+    @classmethod
+    def get_counts(cls) -> Dict[str, int]:
+        """Returns the funciton names and call counts for the decorated functions.
+
+        Returns:
+            Dict[str, int]: Dictionary of function names and counts of calls.
+        """
+        return cls.function_counts
